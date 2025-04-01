e545a2c77159abdbe7f0f2845f00c8d604c0cf60
kennedy.bright <kennedy.bright@nielsen.com>
2025-04-01 17:56:22 -0400

    Test update()

--- app/modules/dbCall/controller/dbCall.controller.js
+++ app/modules/dbCall/controller/dbCall.controller.js
@@ -62,7 +62,7 @@ module.exports = class DBCallController {
       try {
         let dbValues = []
         for (const item of updates) {
-          const dbValue = await this.repo.update(item, { where: {[id]: item[id]}, transaction })
+          const dbValue = await this.repo.update(item, { where: { record_number: item.record_number}, transaction })
           dbValues.push(dbValue)
         }
         await transaction.commit()
